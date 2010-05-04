3754f2b678182783793edb0599217cd411202e13
Mauro Zadunaisky <maurozadu@gmail.com>
2010-05-04 12:04:42 -0300

    adaptated to 1.3

---
+++ webroot/js/brownie.js
@@ -0,0 +1,24 @@
+function hoverRowsColors() {
+   $('tr.list').hover(function() {
+		$(this).css('background-color', '#E1EBF5');
+	},
+	function() {
+		$(this).css('background-color', '#fff');
+	});
+}
+
+function multipleComboSelect() {
+	$('.combo-select').comboselect({ sort: 'none', addbtn: '&raquo;',  rembtn: '&laquo;' });
+}
+
+function bindFancyBox() {
+	$("a[href$=.jpg],a[href$=.jpeg],a[href$=.png],a[href$=.gif]").fancybox({
+		'titlePosition'	:	'inside'
+	});
+}
+
+$(document).ready(function(){
+	multipleComboSelect();
+	hoverRowsColors();
+	bindFancyBox();
+});
