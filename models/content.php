c60fb51d08dc50f1891d1f3d5ab057390e63fd42
Mauro Zadunaisky <maurozadu@gmail.com>
2010-05-12 15:11:01 -0300

    allow Tree and tree as behavior name

--- models/content.php
+++ models/content.php
@@ -178,8 +178,8 @@ class Content extends BrownieAppModel{
 
 
 	function isTree($Model) {
-		//pr($Model->Behaviors->_attached);
-		return in_array('Tree', $Model->Behaviors->_attached) or in_array('tree', $Model->Behaviors->_attached);
+		return in_array('tree', array_map('strtolower', $Model->Behaviors->_attached));
+		//return in_array('Tree', $Model->Behaviors->_attached) or in_array('tree', $Model->Behaviors->_attached);
 	}
 
 
