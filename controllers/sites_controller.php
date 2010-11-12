6cfb74322e68f2603970db57d50f064b2834f16c
Mauro Zadunaisky <maurozadu@gmail.com>
2010-11-12 11:35:55 -0300

    set current site by GET

--- controllers/sites_controller.php
+++ controllers/sites_controller.php
@@ -5,8 +5,15 @@ class SitesController extends BrownieAppController {
 	var $uses = array();
 
 	function select() {
+		$siteId = null;
 		if (!empty($this->data['Site']['id'])) {
-			$this->Session->write('BrwSite', $this->Site->read(null, $this->data['Site']['id']));
+			$siteId = $this->data['Site']['id'];
+		} else if(!empty($this->params['named']['site_id'])) {
+			$siteId = $this->params['named']['site_id'];
+		}
+
+		if ($siteId) {
+			$this->Session->write('BrwSite', $this->Site->read(null, $siteId));
 		} else {
 			$this->Session->delete('BrwSite');
 		}
