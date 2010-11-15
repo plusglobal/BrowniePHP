6474ad337ce1d25961e74da90b21f4da80648e6b
Mauro Zadunaisky <maurozadu@gmail.com>
2010-11-15 17:12:46 -0300

    not sanitize arrays

--- libs/brw_sanitize.php
+++ libs/brw_sanitize.php
@@ -3,6 +3,9 @@
 class BrwSanitize {
 
 	static function html($str) {
+		if (!is_string($str)) {
+			return $str;
+		}
 		return htmlspecialchars($str, ENT_NOQUOTES, Configure::read('App.encoding'));
 	}
 
