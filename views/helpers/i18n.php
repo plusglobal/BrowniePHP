33fca9377c0d5bc2de5a22951f0b29af132c2676
Mauro Zadunaisky <maurozadu@gmail.com>
2011-03-29 15:43:03 -0300

    more languages added

--- views/helpers/i18n.php
+++ views/helpers/i18n.php
@@ -3,11 +3,14 @@
 class i18nHelper extends Helper {
 
 	function humanize($lang) {
+		$out = $lang;
 		switch ($lang) {
+			case 'deu': $out = __d('brownie', 'German', true); break;
 			case 'eng': $out = __d('brownie', 'English', true); break;
-			case 'spa': $out = __d('brownie', 'Spanish', true); break;
+			case 'fra': $out = __d('brownie', 'French', true); break;
 			case 'ita': $out = __d('brownie', 'Italian', true); break;
-			default: $out = $lang; break;
+			case 'nld': $out = __d('brownie', 'Dutch', true); break;
+			case 'spa': $out = __d('brownie', 'Spanish', true); break;
 		}
 		return $out;
 	}
