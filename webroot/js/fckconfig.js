0a7137b0d59628996673f498cf64e866ea2143de
Mauro Zadunaisky <maurozadu@gmail.com>
2011-04-14 09:18:17 -0300

    configured fckeditor to avoid conversion to html entities. close #63

--- webroot/js/fckconfig.js
+++ webroot/js/fckconfig.js
@@ -2,3 +2,7 @@
 	['Bold','Italic','Underline','TextColor','-','Link','Unlink','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','OrderedList','UnorderedList','Table'],
 	['Source','RemoveFormat']
 ] ;
+
+FCKConfig.ProcessHTMLEntities = false; 
+FCKConfig.IncludeLatinEntities = false; 
+FCKConfig.IncludeGreekEntities = false;
