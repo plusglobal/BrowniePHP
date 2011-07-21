63a064efc0484b0756fe1ae0e51a57b581ba37ee
Mauro Zadunaisky <maurozadu@gmail.com>
2011-07-21 15:59:50 -0300

    sanitize url

--- libs/brw_sanitize.php
+++ libs/brw_sanitize.php
@@ -9,4 +9,12 @@ class BrwSanitize {
 		return htmlspecialchars($str, ENT_NOQUOTES, Configure::read('App.encoding'));
 	}
 
+	function url($string) {
+		if (!strtolower(substr($string, 0, 6)) != 'http://' and strtolower(substr($string, 0, 7)) != 'https://') {
+			return 'http://' . $string;
+		} else {
+			return $string;
+		}
+	}
+
 }
