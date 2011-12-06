95c20749eb0ed110fe4c558553c9404090627108
Mauro Zadunaisky <maurozadu@gmail.com>
2011-12-06 17:37:42 -0300

    routes

---
+++ Config/routes.php
@@ -0,0 +1,6 @@
+<?php
+Router::connect('/download/*', array('plugin' => 'brownie', 'controller' => 'downloads', 'action' => 'get'));
+Router::connect('/img/*', array('plugin' => 'brownie', 'controller' => 'thumbs', 'action' => 'view'));
+Router::connect('/thumbs/*', array('plugin' => 'brownie', 'controller' => 'thumbs', 'action' => 'generate'));
+Router::connect('/admin', array('plugin' => 'brownie', 'controller' => 'brownie', 'action' => 'login'));
+Router::connect('/admin/:controller/:action/*', array('plugin' => 'brownie'));
