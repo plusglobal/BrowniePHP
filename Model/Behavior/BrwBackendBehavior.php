0742a29f05ff060af0586e700bcff96129b2b2c7
Mauro Zadunaisky <maurozadu@gmail.com>
2012-06-07 17:15:12 -0300

    custom query for usertype

--- Model/Behavior/BrwBackendBehavior.php
+++ Model/Behavior/BrwBackendBehavior.php
@@ -14,13 +14,18 @@ class BrwBackendBehavior extends ModelBehavior {
 		$authModel = AuthComponent::user('model');
 		$authId = AuthComponent::user('id');
 		if ($authModel and $authModel != 'BrwUser' and !empty($Model->brwConfigPerAuthUser[$authModel])) {
-			if ($Model->brwConfigPerAuthUser[$authModel]['type'] == 'owned') {
-				if ($Model->name == $authModel) {
-					$query['conditions'][$Model->name . '.id'] = $authId;
-				} elseif (!empty($Model->cachedBelongsTo[$authModel])) {
-					$fk = $Model->cachedBelongsTo[$authModel]['foreignKey'];
-					$query['conditions'][$Model->name . '.' . $fk] = $authId;
-				}
+			switch ($Model->brwConfigPerAuthUser[$authModel]['type']) {
+				case 'owned':
+					if ($Model->name == $authModel) {
+						$query['conditions'][$Model->name . '.id'] = $authId;
+					} elseif (!empty($Model->cachedBelongsTo[$authModel])) {
+						$fk = $Model->cachedBelongsTo[$authModel]['foreignKey'];
+						$query['conditions'][$Model->name . '.' . $fk] = $authId;
+					}
+				break;
+				case 'custom':
+					$query = Set::merge($query, $Model->brwCustomQuery($authModel, $authId));
+				break;
 			}
 		}
 		return $query;
